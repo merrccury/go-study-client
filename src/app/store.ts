69a04827bda848f8483255d399298cddfd7cc03b
merrc <merrccury.xz@gmail.com>
2022-05-09 17:37:01 +0300

    Added startup components

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,16 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import calendarReducer from '../features/calendar/calendarSlice';
+import dashboardReducer from '../features/dashboard/dashboardSlice';
+import eventTableReducer from '../features/eventTable/eventTableSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
-  },
+    calendar: calendarReducer,
+    dashboard: dashboardReducer,
+    eventTable: eventTableReducer
+  }
 });
 
 export type AppDispatch = typeof store.dispatch;
